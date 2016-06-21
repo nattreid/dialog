c0b3080e4ba5c16f90a0833ddd59bc0a6d9d930d
Attreid <attreid@gmail.com>
2016-06-21 14:51:17 +0200

    oprava js

--- client-side/src/dialog.js
+++ client-side/src/dialog.js
@@ -1,40 +1,45 @@
-/* ****************************** dialog.js ********************************* */
-
-function Dialog() {
-}
-
-Dialog.redrawOnResize = function (componentId) {
-    Dialog.redraw(componentId);
-    $(window).on('resize.dialogResize', function () {
-        Dialog.redraw(componentId);
-    });
-};
-
-Dialog.redraw = function (componentId) {
-    var dialog = $('#' + componentId);
-    var box = dialog.find('.dialog-box');
-    if (dialog.is(':visible')) {
-        if (box.data('fixed')) {
-            box.centerFixed();
-        } else {
-            box.center();
-        }
-    } else {
-        dialog.css({visibility: 'hidden', display: 'block'});
-
-        if (box.data('fixed')) {
-            box.centerFixed();
-        } else {
-            box.center();
-        }
-        dialog.css({visibility: '', display: 'none'});
-        $('#' + componentId).fadeIn();
+(function ($, window) {
+    if (window.jQuery === undefined) {
+        console.error('Plugin "jQuery" required by "dialog.js" is missing!');
+        return;
     }
-};
 
-$(document).ready(function () {
-    $(document).on('click', '.dialog-container .dialog-background, .dialog-container .dialog-box .dialog-close, .dialog-container .dialog-box .close', function () {
-        $(this).closest('.dialog-container').fadeOut();
-        $(window).off('resize.dialogResize');
+    window.Dialog = {};
+
+    window.Dialog.redrawOnResize = function (componentId) {
+        window.Dialog.redraw(componentId);
+        $(window).on('resize.dialogResize', function () {
+            window.Dialog.redraw(componentId);
+        });
+    };
+
+    window.Dialog.redraw = function (componentId) {
+        var dialog = $('#' + componentId);
+        var box = dialog.find('.dialog-box');
+        if (dialog.is(':visible')) {
+            if (box.data('fixed')) {
+                box.centerFixed();
+            } else {
+                box.center();
+            }
+        } else {
+            dialog.css({visibility: 'hidden', display: 'block'});
+
+            if (box.data('fixed')) {
+                box.centerFixed();
+            } else {
+                box.center();
+            }
+            dialog.css({visibility: '', display: 'none'});
+            $('#' + componentId).fadeIn();
+        }
+    };
+
+    $(document).ready(function () {
+        $(document).on('click', '.dialog-container .dialog-background, .dialog-container .dialog-box .dialog-close, .dialog-container .dialog-box .close', function () {
+            $(this).closest('.dialog-container').fadeOut();
+            $(window).off('resize.dialogResize');
+        });
     });
-});
+
+})(jQuery, window);
