5e57ed479dff04ebea5346a68467ce7aedfd1bc7
Attreid <attreid@gmail.com>
2016-08-24 13:50:26 +0200

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp'),
+        less = require('gulp-less'),
+        minify = require('gulp-clean-css'),
+        uglify = require('gulp-uglify'),
+        rename = require('gulp-rename');
+
+var path = './assets/';
+
+gulp.task('js', function () {
+    return gulp.src(path + 'dialog.js')
+            .pipe(rename({suffix: '.min'}))
+            .pipe(uglify())
+            .pipe(gulp.dest(path));
+});
+
+gulp.task('less', function () {
+    return gulp.src(path + 'dialog.less')
+            .pipe(rename({suffix: '.min'}))
+            .pipe(less())
+            .pipe(minify({keepSpecialComments: 0}))
+            .pipe(gulp.dest(path));
+});
+
+gulp.task('watch', function () {
+    gulp.watch(path + 'dialog.js', ['js']);
+    gulp.watch(path + 'dialog.less', ['less']);
+});
+
+gulp.task('default', ['js', 'less', 'watch']); 
