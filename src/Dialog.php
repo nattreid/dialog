429a79bd1da435845a5972ae5106503c5313ad6a
attreid <attreid@gmail.com>
2016-09-06 10:16:54 +0200

    odsazeni

--- src/Dialog.php
+++ src/Dialog.php
@@ -2,81 +2,92 @@
 
 namespace NAttreid\Dialog;
 
+use Nette\Application\UI\Control;
+
 /**
  * Dialog
- * 
+ *
  * @author Attreid <attreid@gmail.com>
  */
-abstract class Dialog extends \Nette\Application\UI\Control {
+abstract class Dialog extends Control
+{
 
-    /**
-     * @var boolean
-     * @persistent
-     */
-    public $view = FALSE;
+	/**
+	 * @var boolean
+	 * @persistent
+	 */
+	public $view = FALSE;
 
-    /** @var boolean */
-    private $fixed = FALSE;
+	/** @var boolean */
+	private $fixed = FALSE;
 
-    /** @var boolean */
-    private $redrawOnResize = TRUE;
+	/** @var boolean */
+	private $redrawOnResize = TRUE;
 
-    public function handleOpen() {
-        $this->open();
-    }
+	public function handleOpen()
+	{
+		$this->open();
+	}
 
-    public function handleClose() {
-        $this->close();
-    }
+	public function handleClose()
+	{
+		$this->close();
+	}
 
-    /**
-     * Otevreni dialogu
-     */
-    public function open() {
-        $this->view = TRUE;
-        $this->redrawControl('dialog');
-    }
+	/**
+	 * Otevreni dialogu
+	 */
+	public function open()
+	{
+		$this->view = TRUE;
+		$this->redrawControl('dialog');
+	}
 
-    /**
-     * Znovunacteni dialogu
-     */
-    public function refresh() {
-        $this->view = TRUE;
-        $this->redrawControl('dialogContent');
-    }
+	/**
+	 * Znovunacteni dialogu
+	 */
+	public function refresh()
+	{
+		$this->view = TRUE;
+		$this->redrawControl('dialogContent');
+	}
 
-    /**
-     * Zavreni dialogu
-     */
-    public function close() {
-        $this->view = FALSE;
-        $this->redrawControl('dialog');
-    }
+	/**
+	 * Zavreni dialogu
+	 */
+	public function close()
+	{
+		$this->view = FALSE;
+		$this->redrawControl('dialog');
+	}
 
-    /**
-     * Nastavi fixni zobrazeni
-     */
-    public function fixed() {
-        $this->fixed = TRUE;
-    }
+	/**
+	 * Nastavi fixni zobrazeni
+	 */
+	public function fixed()
+	{
+		$this->fixed = TRUE;
+	}
 
-    /**
-     * Nastavi prekreslovani pri zmene rozliseni
-     * @param boolean $redraw
-     */
-    public function redrawOnResize($redraw = TRUE) {
-        $this->redrawOnResize = $redraw;
-    }
+	/**
+	 * Nastavi prekreslovani pri zmene rozliseni
+	 * @param boolean $redraw
+	 */
+	public function redrawOnResize($redraw = TRUE)
+	{
+		$this->redrawOnResize = $redraw;
+	}
 
-    public function render() {
-        $this->template->layout = __DIR__ . '/dialog.latte';
-        $this->template->componentId = $this->getUniqueId();
+	public function render()
+	{
+		$this->template->layout = __DIR__ . '/dialog.latte';
+		$this->template->componentId = $this->getUniqueId();
 
-        $this->template->view = $this->view;
-        $this->template->fixed = $this->fixed;
-        $this->template->redrawOnResize = $this->redrawOnResize;
+		$this->template->view = $this->view;
+		$this->template->fixed = $this->fixed;
+		$this->template->redrawOnResize = $this->redrawOnResize;
 
-        $this->template->render();
-    }
+		$this->template->render();
+	}
 
 }
